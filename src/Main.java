14110abf70a83192e03d7070f33abaaeeb579b2d
Alex Shopiak <alshop2004@gmail.com>
2023-11-28 02:31:19 +0000

    Refactor Main class

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,12 @@ import util.Printer;
 public class Main {
     public static void main(String[] args) {
         byte[][] initMatrix = Inquirer.inquire();
+        Printer.printMatrix("Matrix C", initMatrix);
 
         byte[][] tMatrix = Calculator.transposeMatrix(initMatrix);;
         int sum = Calculator.calculateSum(tMatrix);
 
-        Printer.printMatrix(tMatrix);
+        Printer.printMatrix("Matrix C^T", tMatrix);
         Printer.printSum(sum);
     }
 }
